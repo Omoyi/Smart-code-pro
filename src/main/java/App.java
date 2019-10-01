e9593fe8825147cd82f6480b7c910b16bc7ac769
Nyirate <rwakaemma34@gmail.com>
2019-10-01 19:02:39 +0200

    add new codes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,18 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "team.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
