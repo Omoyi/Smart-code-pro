323d309701d9ee8766bc475b30d9bf94efa51a82
Omoyi <urakhel7@gmail.com>
2019-10-02 09:26:34 +0200

    create a css folder

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+    }
+
 }
