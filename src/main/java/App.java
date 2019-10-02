39b479d0238eb4e2f5e45aa19e75d48b9892892d
Rachel 07 <52781993+Omoyi@users.noreply.github.com>
2019-10-02 10:15:21 +0200

    update according to the master

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,27 @@
 public class App {
+  import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
+public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/home", (request, response) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        },new HandlebarsTemplateEngine());
+    }
 }
