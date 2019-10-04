7809af093f46d26831ce926279535649a8202163
Nyirate <rwakaemma34@gmail.com>
2019-10-04 14:46:28 +0200

    add all

--- src/main/java/DB2.java
+++ src/main/java/DB2.java
@@ -2,5 +2,5 @@ import org.sql2o.*;
 
 public class
 DB2 {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/smart_code", "emmy", "12345");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/smart_code", "emmy", "123456");
 }
